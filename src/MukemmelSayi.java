e79c6ce291107ba91f91671f72c500f824f1d66c
furkangelensoy <134130366+furkangelensoy@users.noreply.github.com>
2023-06-04 13:07:14 +0300

    MukemmelSayi added

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number,total = 0;
+
+        System.out.print("Sayı Giriniz : ");
+        number = input.nextInt();
+
+        for (int i =1;i<number;i++){
+            if (number % i == 0){
+                total += i;
+            }
+        }
+
+        if (total == number){
+            System.out.println(number + " Mükemmel Sayıdır.");
+        }else {
+            System.out.println(number + " Mükemmel Sayı Değildir.");
+        }
+    }
+}
